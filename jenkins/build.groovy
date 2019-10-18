7005f495c21e310350912aceb4c311d8294fffd0
Grzegorz Lyczba <grzegorz.lyczba@gmail.com>
2019-10-18 18:38:58 +0200

    switch to artifactory in GCP, add jenkins pipelines

---
+++ jenkins/build.groovy
@@ -0,0 +1,37 @@
+def label = UUID.randomUUID().toString()
+def projectName = "SSRTBPriceCrypter"
+podTemplate(
+  inheritFrom: 'default',
+  label: label,
+  containers: [
+    containerTemplate(
+      name: 'maven',
+      image: 'maven:3-jdk-8-slim',
+      ttyEnabled: true,
+      command: '/bin/cat',
+      resourceRequestCpu: '4',
+      resourceRequestMemory: '2G',
+      resourceLimitCpu: '4',
+      resourceLimitMemory: '3.5G'
+    )
+]){
+  node(label) {
+    stage ('checkout') {
+      git(
+        url: "${gitSshUrl}",
+        credentialsId: "${gitCredentialsId}",
+        branch: "${gitBranch}"
+      )
+    }
+    stage ('build/test/publish') {
+      container(name: 'maven', shell: '/bin/sh') {
+        sh """#!/bin/sh
+		mvn -B -DskipTests clean package
+		mvn -B test
+		mvn -B install
+		mvn -B publish
+        """
+      }
+    }
+  }
+}
